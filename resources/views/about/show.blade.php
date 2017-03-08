bef5e15fea727bfe77118ed1d85f3b89fe99235a
Niko Bentley <nikobentley@gmail.com>
2017-03-08 11:30:11 -0400

    Edited Nick out of the About page

--- resources/views/about/show.blade.php
+++ resources/views/about/show.blade.php
@@ -8,13 +8,12 @@
                     <div class="panel-heading">About Next JENN</div>
 
                     <div class="panel-body">
-                        Next JENN is made up of then four Database Analytics Developer students listed
+                        Next JENN is made up of then three Database Analytics Developer students listed
                         below. We came together to complete our Capstone project and also delve into an
                         unfamiliar technology to help expand our knowledge base.
                         <ul class="jenn-list">
                             <li><a href="#jong">Jon Gander</a></li>
                             <li><a href="#embeau">Emily Beauchamp</a></li>
-                            <li><a href="#nickb">Nick Buch</a></li>
                             <li><a href="#nikob">Niko Bentley</a></li>
                         </ul>
 
@@ -42,20 +41,6 @@
             <div class="col-md-10 col-md-offset-1">
                 <div id="embeau" class="panel panel-default">
                     <div class="panel-heading">Emily Beauchamp</div>
-
-                    <div class="panel-body">
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div id="nickb" class="panel panel-default">
-                    <div class="panel-heading">Nick Buch</div>
 
                     <div class="panel-body">
 
