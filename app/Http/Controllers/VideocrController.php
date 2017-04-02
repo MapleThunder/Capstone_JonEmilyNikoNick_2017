1b2b20bc2f444466c144c7c67ce1ca218cb6a7a5
Niko Bentley <nikobentley@gmail.com>
2017-04-02 20:47:17 -0300

    more work on image ocr

--- app/Http/Controllers/VideocrController.php
+++ app/Http/Controllers/VideocrController.php
@@ -56,8 +56,58 @@ class VideocrController extends Controller
 
   }
 
-  public function readImage(Request $request)
-  {
+    public function readImage(Request $request)
+    {
+        $target_dir = __DIR__ . '/../../../uploads/';
+        $target_file = $_FILES["imgUp"]["name"];
+
+        $text = (new TesseractOCR($_FILES["imgUp"]))->run();
+
+        var_dump($text); die();
+
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"]))
+        {
+            $check = getimagesize($_FILES["imgUp"]["tmp_name"]);
+            if($check !== false)
+            {
+              echo "File is an image - " . $check["mime"] . ".";
+              $uploadOk = 1;
+            }
+            else
+            {
+              echo "File is not an image.";
+              $uploadOk = 0;
+            }
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        }
+        else
+        {
+            if (move_uploaded_file($_FILES["imgUp"]["tmp_name"], $target_file))
+            {
+                echo "The file ". basename( $_FILES["imgUp"]["name"]). " has been uploaded.";
+            }
+            else
+            {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+
+
+
       if (isset($request->imgUp))
       {
           $imageName = 'tmpImg.png';
