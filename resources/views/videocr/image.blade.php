40a6e3ac6c2b845dfe25ef521d7991521dfa7795
Emily Beauchamp <w0292081@nscc.ca>
2017-04-08 12:48:46 -0300

    Tweet button working. Copy to clipboard button working. Cannot test if email button works but it should... Try it! link and back button on that page do not work.

--- resources/views/videocr/image.blade.php
+++ resources/views/videocr/image.blade.php
@@ -8,50 +8,39 @@
                     <div class="panel-heading">Text in Image</div>
                         <div class="panel-body">
 
-                            <div class="container-fluid">
-
-                                <div class="row">
-                                    <div class="col-md-4">.col-md-4</div>
-                                    <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
-                                </div>
-
-                                <div class="row">
-
-                                    <div class="col-md-6>" style="background-color:yellow;">
-                                        <textarea id="textarea" cols="50" rows="10">{{ $text }}</textarea>
-                                        <br /><br />
-                                        <a class="btn btn-primary" href="videocr">Back</a>
-                                    </div>
-
-
-                                    <div class="col-md-6 col-md-offset-6" style="background-color:pink;">
-
-                                        {{--<button type="button" class="btn btn-primary" onclick="ClipBoard({{ $text }});">Copy</button>--}}
-                                        <button type="button" class="btn btn-primary" onclick="copyToClipboard(document.getElementById('textarea').innerHTML)">Copy</button>
-
-                                        <br /><br />
-
-                                        <a href="mailto:?subject=Check this out!" class="btn btn-primary">Email</a>
-
-                                        <br /><br />
-
-                                        <a class="twitter-share-button btn btn-primary"
-                                           href="https://twitter.com/intent/tweet?text={{ $text }}"
-                                           target="_blank"
-                                           data-size="large">
-                                            Tweet</a>
-
-
-                                    </div>
-
-                                </div>
+                            <div class="col-md-6">
+                                <textarea id="textarea" name="textarea" cols="50" rows="10">{{ $text }}</textarea>
+                                <br /><br />
+                                <a class="btn btn-primary" href="videocr">Back</a>
                             </div>
+
+
+                            <div class="col-md-6">
+
+                                {{--<button id="copyBtn" name="copyBtn" type="button" class="btn btn-primary" onclick="copyToClipboard(document.getElementById('textarea').innerHTML)">Copy</button>--}}
+                                <button id="copyBtn" name="copyBtn" type="button" class="btn btn-primary" onclick="copyToClipboard('textarea')">Copy</button>
+
+                                <br /><br />
+
+                                <a href="mailto:?subject=Check this out!" class="btn btn-primary">Email</a>
+
+                                <br /><br />
+
+                                <a class="twitter-share-button btn btn-primary"
+                                   href="https://twitter.com/intent/tweet?text={{ $text }}"
+                                   target="_blank"
+                                   data-size="large">
+                                    Tweet</a>
+
+                            </div>
+
                         </div>
 
                 </div>
             </div>
         </div>
     </div>
+
 
     {{--BELOW FROM: https://dev.twitter.com/web/javascript/loading--}}
     <script>window.twttr = (function(d, s, id) {
@@ -72,9 +61,12 @@
         }(document, "script", "twitter-wjs"));</script>
 
 
+    {{--BELOW FROM: http://stackoverflow.com/questions/3739330/copy-textarea-text-to-clipboard-html-javascript--}}
     <script>
-        function copyToClipboard(text) {
-            window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+        function copyToClipboard(id) {
+//            window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+            document.getElementById('textarea').select();
+            document.execCommand('copy');
         }
     </script>
 
